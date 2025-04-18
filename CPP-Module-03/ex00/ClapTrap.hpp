c3dc71d08a2d0711087f196ce12a60f3287bb631
Es-salhi Mohammed Amine <117828513+Pilot-61@users.noreply.github.com>
2025-04-18 23:22:49 +0000

    Virtual methods

--- CPP-Module-03/ex00/ClapTrap.hpp
+++ CPP-Module-03/ex00/ClapTrap.hpp
@@ -18,8 +18,8 @@ public:
     ClapTrap(std::string name);
     ClapTrap(const ClapTrap& other);
     ClapTrap& operator=(const ClapTrap& other);
-    ~ClapTrap();
-    void attack(const std::string& target);
+    virtual ~ClapTrap();
+    virtual void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
 };
--- CPP-Module-03/ex01/ClapTrap.hpp
+++ CPP-Module-03/ex01/ClapTrap.hpp
@@ -18,7 +18,7 @@ public:
     ClapTrap(std::string name);
     ClapTrap(const ClapTrap& other);
     ClapTrap& operator=(const ClapTrap& other);
-    ~ClapTrap();
+    virtual ~ClapTrap();
     virtual void attack(const std::string& target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
